3c9c22ce05602b64c685c6d5e35298c2be406cac
bverjat <qwsx@dcfvg.com>
2016-07-20 20:16:30 +0200

    warning on reload

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,14 @@ const Papa = require('papaparse')
 export default {
   data: function(){ return {state} },
   created: function () {
-    // window.addEventListener('keyup', this.keyHandler);
-    // window.addEventListener("beforeunload", function (e) {
-    //     var confirmationMessage = 'It looks like you have been editing something. '
-    //                             + 'If you leave before saving, your changes will be lost.';
+    window.addEventListener('keyup', this.keyHandler);
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = 'It looks like you have been editing something. '
+                                + 'If you leave before saving, your changes will be lost.';
 
-    //     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    //     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-    // });
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
   },
   components: {Loader, Tweet, Controls, Infobar, Hints },
   computed:{
