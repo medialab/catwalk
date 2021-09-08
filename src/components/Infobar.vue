683af76b6fc67a709db1171f837c6d8d170a3758
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-09-08 12:09:08 +0200

    improve rendering of deleted tweets by fixing some default fieldnames + adding links to user & tweet

--- src/components/Infobar.vue
+++ src/components/Infobar.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="navbar navbar-fixed-bottom container-fluid">
         <p class="col-sm-3">
-          <img v-bind:src="[tweet.from_user_profile_image_url]">
-          <strong >@{{tweet.from_user_name}}</strong>
+          <a target="_blank" :href="[('https://twitter.com/' + (tweet.from_user_screenname || tweet.user_screen_name))]">
+            <img v-bind:src="[(tweet.from_user_profile_image_url || tweet.user_image).replace(/^https?:/, '')]">
+            <strong>@{{tweet.from_user_screenname || tweet.user_screen_name || tweet.from_user_name || tweet.user_name}}</strong>
+          </a>
         </p>
-        <p class="col-sm-7">{{tweet.text}}</p>
+        <p class="col-sm-7">{{tweet.full_text || tweet.text}}</p>
+        <p class="col-sm-7"><a target="_blank" :href="[(tweet.url || 'https://twitter.com/' + (tweet.from_user_screenname || tweet.user_screen_name) + '/status/' + tweet.id)]">&#128279; link</a></p>
     </p>
     </div>
 </template>
