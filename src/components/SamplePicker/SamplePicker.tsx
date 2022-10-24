41c7451c3b9f629c6556baa074612a5122055f47
Yomguithereal <guillaumeplique@gmail.com>
2022-10-24 22:02:41 +0200

    Improving SamplePicker types

--- src/components/SamplePicker/SamplePicker.tsx
+++ src/components/SamplePicker/SamplePicker.tsx
@@ -2,14 +2,21 @@ import Select from 'react-select';
 
 import {useI18nMessages} from '../../hooks';
 
-function SamplePicker({options, onChange}) {
+type SamplePickerOption = {value: string; label: string};
+
+interface SamplePickerProps {
+  options: Array<SamplePickerOption>;
+  onChange?: (sampleName: string) => void;
+}
+
+function SamplePicker({options, onChange}: SamplePickerProps) {
   const {chooseADataSample, chooseADataSamplePlaceholder} = useI18nMessages();
   return (
     <div className="SamplePicker">
       <div>{chooseADataSample}</div>
       <div>
         <Select
-          onChange={onChange}
+          onChange={e => onChange(e.value)}
           options={options}
           isSearchable={false}
           placeholder={chooseADataSamplePlaceholder}
