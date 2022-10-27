2d3b6dd711152071fd641fa4ba4aa0e7a62e5849
Yomguithereal <guillaumeplique@gmail.com>
2022-10-27 13:44:24 +0200

    Moving AnnotationView-related components from Application

--- src/components/Application/AnnotationView.tsx
+++ src/components/Application/AnnotationView.tsx
@@ -1,6 +1,61 @@
 import MediaPreview from '../MediaPreview';
 import DownloadFooter from '../DownloadFooter';
+import Railway, {HiddenRailway} from '../Railway';
+import TagsColumn, {HiddenTagsColumn} from '../TagsColumn';
 import {useCSVData, useAnnotationConfig, useDisplayModal} from '../../hooks';
+import {DEFAULT_ANNOTATION_SORT_ORDER} from '../../defaults';
+
+export function RailwayWrapper({isShown = false}) {
+  const [csvData] = useCSVData();
+  const [annotationConfig] = useAnnotationConfig();
+
+  if (!isShown) return <HiddenRailway />;
+
+  if (!csvData)
+    throw new Error(
+      'It should not be possible to display Railway without data being loaded!'
+    );
+
+  if (!annotationConfig)
+    throw new Error(
+      'It should not be possible to display Railway without an annotation config!'
+    );
+
+  return (
+    <Railway
+      rows={csvData.rows}
+      schema={annotationConfig.schema}
+      navKeyBindings={annotationConfig.options.navKeyBindings}
+      activeObjectIndex={0}
+      sortOrder={DEFAULT_ANNOTATION_SORT_ORDER}
+    />
+  );
+}
+
+export function TagsColumnWrapper({isShown = false}) {
+  const [csvData] = useCSVData();
+  const [annotationConfig, annotationStats] = useAnnotationConfig();
+
+  if (!isShown) return <HiddenTagsColumn />;
+
+  if (!csvData)
+    throw new Error(
+      'It should not be possible to display Railway without data being loaded!'
+    );
+
+  if (!annotationConfig || !annotationStats)
+    throw new Error(
+      'It should not be possible to display Railway without an annotation config!'
+    );
+
+  return (
+    <TagsColumn
+      schema={annotationConfig.schema}
+      stats={annotationStats}
+      total={csvData.rows.length}
+    />
+  );
+}
 
 export default function AnnotationView() {
   const [csvData] = useCSVData();
--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -3,7 +3,10 @@ import {useState} from 'react';
 import type {View} from '../../types';
 import LandingView from './LandingView';
 import DataPreviewView from './DataPreviewView';
-import AnnotationView from './AnnotationView';
+import AnnotationView, {
+  RailwayWrapper,
+  TagsColumnWrapper
+} from './AnnotationView';
 import {LangContext} from '../../contexts';
 import Layout from '../Layout/Layout';
 import MainColumn from '../Layout/MainColumn';
@@ -11,64 +14,8 @@ import MainRow from '../Layout/MainRow';
 import Header from '../Header';
 import Footer from '../Footer';
 import Modals from '../Modals';
-import Railway, {HiddenRailway} from '../Railway';
-import TagsColumn, {HiddenTagsColumn} from '../TagsColumn';
-import {useCSVData, useAnnotationConfig} from '../../hooks';
-import {DEFAULT_ANNOTATION_SORT_ORDER} from '../../defaults';
 
 import '../../styles/entrypoint.scss';
-
-function RailwayWrapper({isShown = false}) {
-  const [csvData] = useCSVData();
-  const [annotationConfig] = useAnnotationConfig();
-
-  if (!isShown) return <HiddenRailway />;
-
-  if (!csvData)
-    throw new Error(
-      'It should not be possible to display Railway without data being loaded!'
-    );
-
-  if (!annotationConfig)
-    throw new Error(
-      'It should not be possible to display Railway without an annotation config!'
-    );
-
-  return (
-    <Railway
-      rows={csvData.rows}
-      schema={annotationConfig.schema}
-      navKeyBindings={annotationConfig.options.navKeyBindings}
-      activeObjectIndex={0}
-      sortOrder={DEFAULT_ANNOTATION_SORT_ORDER}
-    />
-  );
-}
-
-function TagsColumnWrapper({isShown = false}) {
-  const [csvData] = useCSVData();
-  const [annotationConfig, annotationStats] = useAnnotationConfig();
-
-  if (!isShown) return <HiddenTagsColumn />;
-
-  if (!csvData)
-    throw new Error(
-      'It should not be possible to display Railway without data being loaded!'
-    );
-
-  if (!annotationConfig || !annotationStats)
-    throw new Error(
-      'It should not be possible to display Railway without an annotation config!'
-    );
-
-  return (
-    <TagsColumn
-      schema={annotationConfig.schema}
-      stats={annotationStats}
-      total={csvData.rows.length}
-    />
-  );
-}
 
 export default function Application() {
   const [view, setView] = useState<View>('landing');
